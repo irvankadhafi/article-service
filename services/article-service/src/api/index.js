522619c160e54935a0d043c712cd15232952b016
Irvan Kadhafi <irvan.kadhafi.tif18@polban.ac.id>
2021-09-15 17:40:23 +0700

    fixing docker volume

--- services/article-service/src/api/index.js
+++ services/article-service/src/api/index.js
@@ -7,6 +7,8 @@ router.get('/', (req, res) => {
   res.json({
     name: 'Hello World! My name Irvan Kadhafi',
     message: 'This is Article Service',
+    endpoint: '[POST] /article => to post a new article',
+    requestBody: '{author, title, body}',
   });
 });
 router.use('/article', article);
